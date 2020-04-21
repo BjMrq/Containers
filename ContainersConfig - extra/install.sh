4b0693fb7eed8bb22df416b78f9f3f434554dcc0
Benjamin Marquis <bmarquiscom@gmail.com>
2020-04-21 08:58:13 -0400

    Add an install script that shoul take care of setting the environement in place for new dev

--- ContainersConfig - extra/install.sh
+++ ContainersConfig - extra/install.sh
@@ -4,3 +4,4 @@ cp .env.example .env
 docker run --rm -v $(pwd):/app composer install
 docker-compose up --build -d
 docker-compose exec app php artisan migrate
+docker-compose exec app php artisan db:seed
