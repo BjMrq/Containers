db6b216b032d0aedb54dd3ebc91110b924f673fe
Benjamin <bmarquiscom@gmail.com>
2020-06-25 18:15:33 -0400

    Update install

--- ContainersConfig - extra/install.sh
+++ ContainersConfig - extra/install.sh
@@ -4,21 +4,30 @@ cd ..
 
 ENV=./.env
 if ! test -f "$ENV"; then
-  echo "Copying environement variables.."
+  echo "Copying environement variables ⚙️"
   cp .env.example .env
 fi
 
-echo "Installing Composer dependencies.."
+
+echo "Installing Composer dependencies 🛠"
 docker run --rm -v $(pwd):/var/www --env COMPOSER_MEMORY_LIMIT=-1 \
   bjmrq/laravel:php-7.4 composer install
 
-echo "Building Containers.."
+echo "Updating Composer 🔨"
+docker run --rm -v $(pwd):/var/www --env COMPOSER_MEMORY_LIMIT=-1 \
+  bjmrq/laravel:php-7.4 composer update
+
+
+echo "Building Docker Containers 🐳"
+cd ..
 docker-compose up --build -d
 
+
+cd app/
 DATABASE=./ContainersConfig/mysql/databases/database_snapshot.sql
 if test -f "$DATABASE"; then
-  echo "Importing database.."
+  echo "Importing database 📥"
   docker-compose exec -T db mysql -u user --password=password project_db < ./ContainersConfig/mysql/databases/database_snapshot.sql
 fi
 
-echo "Installation complete"
+echo "Installation complete 🚀"
